1b3e45883d80c06c41f166169381f2de67c78a0d
Reyann Larkey <reyannlarkey@gmail.com>
2020-02-05 12:24:15 -0700

    it can solve easy puzzles now

--- Sudoku/Code/sudoku_solver.py
+++ Sudoku/Code/sudoku_solver.py
@@ -14,11 +14,11 @@ Three places to check:
 '''
 
 
-df = pd.read_csv('../Data/sudoku.csv', nrows = 1)
+df = pd.read_csv('../Data/sudoku.csv', nrows = 2)
 
 
-quiz = np.asarray([int(i) for i in df.quizzes.values[0]]) # convert to array of numbers
-solution = np.asarray([int(i) for i in df.solutions.values[0]]) # convert to array of numbers
+quiz = np.asarray([int(i) for i in df.quizzes.values[1]]) # convert to array of numbers
+solution = np.asarray([int(i) for i in df.solutions.values[1]]) # convert to array of numbers
 # print(quiz)
 
 puzzle = quiz.reshape((9,9))
@@ -82,62 +82,98 @@ class Game:
 
     def get_possible_locations(self, number):
         ''' returns mask with possible locations for a given number'''
-
-        # get empty locations first:
-        empty_locations = (self.puzzle ==0)# locations of empty numbers
-
-        possible_locations = empty_locations # to start
-
-        # check rows:
-        for i, row in enumerate(self.puzzle):
-            if number in row:
-                possible_locations[i] = False
-
-        # check columns:
-        for i in range(9):
-            column = self.get_column(x = i)
-            if number in column:
-                possible_locations[:,i] = False
-
-        # check blocks:
-        for i in range(8):
-
-            block, y_start, x_start = self.get_block(i)
-
-            if number in block:
-                possible_locations[y_start:y_start+3, x_start:x_start+3] = False
-
-        print(number)
-        print(possible_locations)
-        self.found_new= False
-        for i in range(9):
-            block, y_start, x_start = self.get_block(i, arr=possible_locations)
-            if block.sum()==1:
-
-                y_pos, x_pos = np.where(block == 1)
-                y_pos+=y_start
-                x_pos+=x_start
-
-                self.puzzle[y_pos,x_pos] = number
-                print(f"Found where a {number} goes!: {x_pos, y_pos}")
-                self.found_new = True
-
         unique, counts = np.unique(self.puzzle, return_counts=True)
 
+        finished_nums = np.where(counts[1:] == 9)[0]+1
+
+
+
+        if number in finished_nums:
+            return dict(zip(unique, counts))
+
+        else:
+            # print(finished_nums)
+            # print(number)
+            # get empty locations first:
+            empty_locations = (self.puzzle ==0)# locations of empty numbers
+
+            possible_locations = empty_locations # to start
+
+            # check rows:
+            for i, row in enumerate(self.puzzle):
+                if number in row:
+                    possible_locations[i] = False
+
+            # check columns:
+            for i in range(9):
+                column = self.get_column(x = i)
+                if number in column:
+                    possible_locations[:,i] = False
+
+            # check blocks:
+            for i in range(9):
+
+                block, y_start, x_start = self.get_block(i)
+
+                if number in block:
+                    possible_locations[y_start:y_start+3, x_start:x_start+3] = False
+
+            # print(number)
+            # print(possible_locations)
+            self.found_new= False
+            for i in range(9):
+                block, y_start, x_start = self.get_block(i, arr=possible_locations)
+                if block.sum()==1:
+
+                    y_pos, x_pos = np.where(block == 1)
+                    y_pos+=y_start
+                    x_pos+=x_start
+
+                    self.puzzle[y_pos,x_pos] = number
+                    # print(f"Found where a {number} goes!: {x_pos, y_pos}")
+                    self.found_new = True
+
+            unique, counts = np.unique(self.puzzle, return_counts=True)
+
         return dict(zip(unique, counts))
-p = Game(quiz)
-print(p.puzzle)
-# p.get_block(0)
-# p.get_block(1)
-# p.get_block(2)
-# p.get_block(3)
 
 
+df = pd.read_csv('../Data/sudoku.csv', nrows = 2000)
 
-dict1 = p.get_possible_locations(1)
-while p.found_new:
-    for i in range(1,10):
-        dict1 = p.get_possible_locations(i)
-        print(dict1)
+iterations = []
+for j in range(df.shape[0]):
+    quiz = np.asarray([int(i) for i in df.quizzes.values[j]]) # convert to array of numbers
+    solution = np.asarray([int(i) for i in df.solutions.values[j]]) # convert to array of numbers
+    # print(quiz)
 
-print(solution)
+    puzzle = quiz.reshape((9,9))
+    solution = solution.reshape((9,9))
+
+
+    p = Game(quiz)
+    # print(p.puzzle)
+    # p.get_block(0)
+    # p.get_block(1)
+    # p.get_block(2)
+    # p.get_block(3)
+
+
+    # p.get_possible_locations(1)
+
+
+    dict1 = p.get_possible_locations(1)
+    count = 0
+    while not np.array_equal(p.puzzle, solution):
+        for i in range(1,10):
+            count +=1
+            dict1 = p.get_possible_locations(i)
+            # print(dict1)
+    print(count)
+    iterations.append(count)
+    # print(p.puzzle)
+    # print(solution)
+
+
+p, bins = np.histogram(iterations, bins = np.arange(0,100,5))
+plt.step(bins[0:-1], p, where = 'post')
+plt.show()
